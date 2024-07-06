65b69fba4864f411d7cc53dc2e73d43027f25d0e
Matt Layman <matthewlayman@gmail.com>
2024-07-05 22:09:29 -0400

    Switch to loading the module using Lua's `require` function.

--- examples/calllua.c
+++ examples/calllua.c
@@ -7,16 +7,11 @@ int main(void) {
     lua_State *lua_state = luaL_newstate();
     luaL_openlibs(lua_state);
 
-    // Load and compile the script.
-    const char* lua_script = "connection.lua";
-    int status = luaL_loadfile(lua_state, lua_script);
-    if (status != LUA_OK) {
-        printf("Error: %s\n", lua_tostring(lua_state, -1));
-        return 1;
-    }
-
-    // Create the module and add to the top of the Lua stack.
-    status = lua_pcall(lua_state, 0, 1, 0);
+    // Load the module.
+    lua_getglobal(lua_state, "require");
+    const char* module_name = "nibiru_package_test.connection";
+    lua_pushstring(lua_state, module_name);
+    int status = lua_pcall(lua_state, 1, 1, 0);
     if (status != LUA_OK) {
         printf("Error: %s\n", lua_tostring(lua_state, -1));
         lua_close(lua_state);
---
+++ examples/nibiru_package_test/connection.lua
@@ -0,0 +1,9 @@
+local M = {}
+
+function M.handle_connection(data)
+	print(data)
+	print("hello world")
+	return "Hello from Lua required package!"
+end
+
+return M
