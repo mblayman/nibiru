75367ae60c9e1654e63d2ad4b79a2fcb14f369c5
Matt Layman <matthewlayman@gmail.com>
2026-01-02 02:12:27 -0500

    Add slow endpoint for I/O-bound simulation perf testing.

--- docs/app.lua
+++ docs/app.lua
@@ -6,8 +6,15 @@ local function index()
     return http.ok("Nibiru Docs")
 end
 
+local function slow()
+    -- Simulate I/O request with 200ms delay
+    os.execute("sleep 0.2")
+    return http.ok("OK")
+end
+
 local routes = {
     Route("/", index),
+    Route("/slow", slow),
 }
 
 return Application(routes)
